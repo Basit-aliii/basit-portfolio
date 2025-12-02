84e9d66de1b73e3b58b27a198c966bf767bdb0c8
Basit-aliii <Basit-aliii@users.noreply.github.com>
2025-12-02 18:47:24 +0000

    deploy: 69816e7e46316028d29beb28162fa0b7cff745a9

--- _next/static/P3KSlEI1_he4x5PLbtS7b/_buildManifest.js
+++ _next/static/P3KSlEI1_he4x5PLbtS7b/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/basit-portfolio//_next/:path+",
+        "destination": "/basit-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
